0eb1520ea0eec41438538bd00f454276c66b066d
Jim Newsome <jnewsome@torproject.org>
2020-05-17 18:37:18 -0500

    Replace placeholder links

--- pelicanconf.py
+++ pelicanconf.py
@@ -29,14 +29,14 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+#LINKS = (('Pelican', 'http://getpelican.com/'),
+#         ('Python.org', 'http://python.org/'),
+#         ('Jinja2', 'http://jinja.pocoo.org/'),
+#         ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('Mastodon', 'https://mastodon.social/@sporksmith'),
+          ('Twitter', 'https://twitter.com/sporksmith'),)
 
 DEFAULT_PAGINATION = 10
 
